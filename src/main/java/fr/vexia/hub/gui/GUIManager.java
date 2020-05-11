5402d3c13cc9ec697331111319078689abb85989
Spileur <contact@spileur.fr>
2020-05-11 18:05:11 +0200

    fix intentory error

--- src/main/java/fr/vexia/hub/gui/GUIManager.java
+++ src/main/java/fr/vexia/hub/gui/GUIManager.java
@@ -54,13 +54,6 @@ public class GUIManager implements Listener {
                 .title("Hubs")
                 .build();
 
-        this.hostMenu = SmartInventory.builder()
-                .id("host_menu")
-                .provider(new HostGUI(this))
-                .size(4,9)
-                .title("Hosts")
-                .build();
-
         this.profilMenu = SmartInventory.builder()
                 .id("profil_menu")
                 .provider(new ProfilGUI(this))
